47b737275b862d1c6a30554b79e064bfa1f327c4
vishakh-k <vishakhkt.2003@gmail.com>
2025-05-22 15:53:24 +0530

    taskes are added

--- day4/lambda/task3.py
+++ day4/lambda/task3.py
@@ -10,10 +10,14 @@ list1=[1,2,3,4,5,6,7];
 max1=reduce(lambda x,y:x if x>y else y,list1);
 print(max1);"""
 
-from functools import reduce
+"""from functools import reduce
 
 list1 = [1, 2, 3, 4, 5]
 num1 = reduce(lambda x,y : x + y, list1)
 num2 = reduce(lambda x,y : x * y, list1)
-print(num1,num2);
+print(num1,num2);"""
+
+print(list(map(lambda x: x**2, filter(lambda x: x % 2 == 0, range(1, 51)))));
+
+
 
